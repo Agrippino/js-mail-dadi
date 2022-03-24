ae70d23318dda4797954da022dd0306e5c6373ae
Agrippino <agrippinorussiello@outlook.it>
2022-03-24 15:11:39 +0100

    Correzione effettuata, viene stampato una volta

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let inserisciTesto = prompt("Azione necessaria, inserisci tui la tua email");
 let email = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"]
 
 for (let i=0; i < email.length;i++){
-    if(email[i]!=inserisciTesto){
+    if(inserisciTesto[i]!=email){ 
         alert("Mi dispaice ma non è registrato");
-        console.log("non sei regitrato");               
+        console.log("non sei regitrato");  
+        break;        
     }
 }
