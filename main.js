11c340dea2a1c87a6bce6eadcfbc4f62cd0919ec
Agrippino <agrippinorussiello@outlook.it>
2022-03-24 15:27:02 +0100

    Inserite annotazioni

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 
+//Dichiaro le variabili per il programma
 let inserisciTesto = prompt("Azione necessaria, inserisci tui la tua email");
-let emailServer = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"]
+let emailNelServer = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"]
 let emailNonAccettata
-
-for (let i=0; i < emailServer.length;i++){
-    if(inserisciTesto[i]!=emailServer){ 
+//ciclo for per confronto con il server
+for (let i=0; i < emailNelServer.length;i++){
+    if(inserisciTesto[i]!=emailNelServer){ 
         alert( emailNonAccettata="Mi dispaice ma non è registrato");
         console.log("non sei regitrato");  
         break;        
     }
 }
+//reminder fisso in pagina
 document.getElementById("demo").innerHTML = emailNonAccettata;
